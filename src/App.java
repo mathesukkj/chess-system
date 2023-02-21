bdef052174528611689a969a8a00a3660904a840
mathesukkj <kemuel.g7363@gmail.com>
2023-02-21 18:10:14 -0300

    feat: add basic piece movement

--- src/App.java
+++ src/App.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class App {
     public static void main(String[] args) throws Exception {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.print("\nSource: ");
+            ChessPosition source = UI.readChessPosition(s);
+
+            System.out.print("\nTarget: ");
+            ChessPosition target = UI.readChessPosition(s);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
