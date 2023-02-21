677c632fec00836d98051dffeb16210baa42d415
mathesukkj <kemuel.g7363@gmail.com>
2023-02-21 13:54:22 -0300

    feat: add piece method to Board class

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,6 +5,14 @@ public class Board {
     private int columns;
     private Piece[][] pieces;
 
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
@@ -15,16 +23,7 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
-
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
 }
