6f693e7cb9d2868885a162eb0fae800b4c7927c5
mathesukkj <kemuel.g7363@gmail.com>
2023-02-21 13:59:20 -0300

    test: check if UI prints the board

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import chess.ChessMatch;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
