7a1bd3fe2af3d2b23d9088bd997264cbd56d5d33
mathesukkj <kemuel.g7363@gmail.com>
2023-02-22 18:59:21 -0300

    feat: add Knight piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
@@ -193,7 +194,11 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, ColorEnum.WHITE));
+        placeNewPiece('b', 1, new Knight(board, ColorEnum.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, ColorEnum.WHITE));
         placeNewPiece('e', 1, new King(board, ColorEnum.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, ColorEnum.WHITE));
+        placeNewPiece('g', 1, new Knight(board, ColorEnum.WHITE));
         placeNewPiece('h', 1, new Rook(board, ColorEnum.WHITE));
         placeNewPiece('a', 2, new Pawn(board, ColorEnum.WHITE));
         placeNewPiece('b', 2, new Pawn(board, ColorEnum.WHITE));
@@ -203,11 +208,13 @@ public class ChessMatch {
         placeNewPiece('f', 2, new Pawn(board, ColorEnum.WHITE));
         placeNewPiece('g', 2, new Pawn(board, ColorEnum.WHITE));
         placeNewPiece('h', 2, new Pawn(board, ColorEnum.WHITE));
-        placeNewPiece('g', 1, new Bishop(board, ColorEnum.WHITE));
-        placeNewPiece('b', 1, new Bishop(board, ColorEnum.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, ColorEnum.BLACK));
+        placeNewPiece('b', 8, new Knight(board, ColorEnum.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, ColorEnum.BLACK));
         placeNewPiece('e', 8, new King(board, ColorEnum.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, ColorEnum.BLACK));
+        placeNewPiece('g', 8, new Knight(board, ColorEnum.BLACK));
         placeNewPiece('h', 8, new Rook(board, ColorEnum.BLACK));
         placeNewPiece('a', 7, new Pawn(board, ColorEnum.BLACK));
         placeNewPiece('b', 7, new Pawn(board, ColorEnum.BLACK));
@@ -217,8 +224,6 @@ public class ChessMatch {
         placeNewPiece('f', 7, new Pawn(board, ColorEnum.BLACK));
         placeNewPiece('g', 7, new Pawn(board, ColorEnum.BLACK));
         placeNewPiece('h', 7, new Pawn(board, ColorEnum.BLACK));
-        placeNewPiece('g', 8, new Bishop(board, ColorEnum.BLACK));
-        placeNewPiece('b', 8, new Bishop(board, ColorEnum.BLACK));
 
     }
 }
